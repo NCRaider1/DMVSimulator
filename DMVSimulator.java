1d3f669625c4481635b360e13b8b0a492368c3a6
NCRaider1 <nicholas.centeno.ths15@gmail.com>
2023-03-07 14:51:41 -0500

    finished part 1

--- DMVSimulator.java
+++ DMVSimulator.java
@@ -2,10 +2,35 @@ import java.util.Random;
 
 public class DMVSimulator
 {
+    public static void main(String[] args)
+    {
     Random r = new Random();
     int order = r.nextInt(100)+1;
     
     System.out.println("Welcome to the DMV. Please take a number");
+    System.out.println("Your number is " + order + ". Please wait until that number is called");
 
+    int currentSpot = order+1;
+    boolean called = false;
+
+    while(called)
+    {
+        System.out.println("Now calling " + currentSpot + ". Please be patient.");
+        if (currentSpot == order)
+        {
+            called = true;
+        }
+
+        if (called && currentSpot == order)
+        {
+            System.out.println("Now calling " + order + " please come to the desk.");
+            break;
+        }
+
+        currentSpot = (currentSpot % 100)+1;
+    }
+
+    System.out.println("Oh i'm sorry, you do not have the paperwork required.");
+    }
 }
 
