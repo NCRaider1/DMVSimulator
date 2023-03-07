474f6445baf9848beffb11312ca76194bc352750
NCRaider1 <nicholas.centeno.ths15@gmail.com>
2023-03-07 14:41:00 -0500

    new change

--- DMVSimulator.java
+++ DMVSimulator.java
@@ -1,4 +1,11 @@
+import java.util.Random;
+
 public class DMVSimulator
 {
+    Random r = new Random();
+    int order = r.nextInt(100)+1;
     
+    System.out.println("Welcome to the DMV. Please take a number");
+
 }
+
